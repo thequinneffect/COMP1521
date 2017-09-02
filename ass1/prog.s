2cd87f13152fb866c84aa7ab2692232cfbd5d508
thequinneffect <n.quinn@student.unsw.edu.au>
2017-09-02 10:39:10 +1000

    update

--- ass1/prog.s
+++ ass1/prog.s
@@ -6,20 +6,42 @@
 #
 # Written by Nicholas Quinn (z5117408), August 2017
 
-		.data
-main_ret_save:	.space 4
+					.data
 
-		.text
-		.globl main
+N: 					.word 	10 							# changing this value is equivalent to #define NN change
+main_ret_save:		.space 	4 							# allocate 4 bytes for main return address
+prompt: 			.asciiz "# iterations: "
+iterations: 		.word 0
+
+###############################################################################################################
+
+					.text
+					.globl main
 
 main:
-	sw   $ra, main_ret_save
+					sw 		$ra, main_ret_save 			# save main return address
 
-	# Your main program code goes here
+					li 		$v0, 4 						# print prompt
+					la 		$a0, prompt
+					syscall
+
+					li 		$v0, 5						# read in integer
+					syscall
+
+					sw 		$v0, iterations
+
+					j 		end_main
+
 
 end_main:
-	lw   $ra, main_ret_save
-	jr   $ra
+					lw   $ra, main_ret_save 			# load return address of main into return address reg.
+					jr   $ra 							# jump to main caller
 
 
-# The other functions go here
+###############################################################################################################
+
+neighbours:
+
+###############################################################################################################
+
+copy_show:
